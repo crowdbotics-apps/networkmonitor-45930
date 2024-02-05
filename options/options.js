5add7009fc406796ef16812b8474af56376439e3
Crowdbotics <team@crowdbotics.com>
2024-02-05 17:07:22 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "networkmonitor-45930",
+  url: "https://networkmonitor-45930.botics.co",
+  api: "https://networkmonitor-45930.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
